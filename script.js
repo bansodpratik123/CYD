c334a7de7b48334f2c79aab77962c86a4e1c011a
Pratik Bansod <83473640+bansodpratik123@users.noreply.github.com>
2023-07-07 23:47:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+/* script.js */
+
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to be executed after the document is fully loaded
+    console.log('Document loaded.');
+});
